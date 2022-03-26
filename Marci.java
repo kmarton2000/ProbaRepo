3f243a686e450eae5d418afac70e173d7bb794d3
kmarton2000 <75527890+kmarton2000@users.noreply.github.com>
2022-03-26 10:27:22 +0100

    Add files via upload
    Most elméletileg felraktam két fájlt (a ProbaMain-t, és a Marci fájlt), aztán tudja a kurva élet hogy fogom a tietekkel ezt összemergelni, mert 1 órája nézem a videókat, de fingom sincs róla

---
+++ Marci.java
@@ -0,0 +1,5 @@
+public class Marci {
+	public String metodus(String szoveg) {
+		return szoveg+"kopter";
+	}
+}
---
+++ ProbaMain.java
@@ -0,0 +1,12 @@
+
+public class ProbaMain {
+
+	public static void main(String[] args) {
+		Brigi brigi = new Brigi();
+		Dani dani = new Dani();
+		Marci marci = new Marci();
+		System.out.print(brigi.metodus(09).toString()+"\n"+dani.metodus(73).toStrinf()+"\n"+marci.metodus("Alabama"));
+
+	}
+
+}
